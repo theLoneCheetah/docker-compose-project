d9573a096753c7dd6519404ff8e3f18468e2f05f
theLoneCheetah <ils.mozhaev@gmail.com>
2024-01-09 12:19:05 +0300

    Deleted directory

--- version0/client/client.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import urllib.request
-
-with urllib.request.urlopen("http://server:1200/") as url:
-	encoded = url.read()
-	decoded = encoded.decode("utf8")
-	print(decoded)
